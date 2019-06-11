1eb061c334a0c42bd16f1e0f5ece171636d2aaa0
Luis Enrique Ortiz Ramirez <luisortramirez2211@gmail.com>
2019-06-11 18:57:18 -0500

    Nuevos comentarios sobre sintaxis dart

--- dart/01_dart_sintaxis.dart
+++ dart/01_dart_sintaxis.dart
@@ -2,6 +2,25 @@
 // Forma abreviada de creacion de funciones
 // main() => print("Hola mundo");
 
+/**
+ * Tipos de comentarios: /* comentario */
+ /*Este es un comentario mulltilinea
+ */
+
+// Este comentario es de una sola linea
+
+/// Los comentarios de triple diagonal
+/// sirven para documentar funciones, clases o variables
+/// y pueden ser procesados por el programa dartdoc
+/// para generar documentacion.
+/// Ademas estos permiten incluir sintaxis de Markdown, por ejemplo:
+/// # Un titulo
+/// 
+/// ## Subtitulo
+/// 
+/// Una lista de elementos:
+/// - Elemento 1
+/// - Elemento 2
 void main(List<String> argumentos) {
 	// Interpreta el tipo en tiempo de compilacion
 	var str = "Algo";
